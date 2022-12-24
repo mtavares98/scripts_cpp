a3e3ed9faa78d12bd203e7b09eb20ae7190e8c0e
Miguel Silva <miguel1998282@gmail.com>
2022-12-24 16:59:28 +0000

    Added the header generator

---
+++ header_gen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for	i in "$@"
+do
+	echo "#ifndef ${i^^}" > $i.hpp
+	echo "#define ${i^^}_HPP" >> $i.hpp
+	echo "" >> $i.hpp
+	echo "class $i" >> $i.hpp
+	echo "{" >> $i.hpp
+	echo "" >> $i.hpp
+	echo "	public:" >> $i.hpp
+	echo "		$i(void);" >> $i.hpp
+	echo "		$i($i const &copy);" >> $i.hpp
+	echo "		$i &operator=($i const &copy);" >> $i.hpp
+	echo "		~$i(void);" >> $i.hpp
+	echo "};" >> $i.hpp
+	echo "#endif" >> $i.hpp
+done
